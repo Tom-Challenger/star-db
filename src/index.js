a50fbd760ed7a27286a182280ee6a6e49d0e338a
Tom-Challenger <it.mail.art@gmail.com>
2019-07-15 15:25:11 +0300

    Использование Fetch API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+fetch('https://swapi.co/api/people/1/')
+	.then((res) => {
+		console.log('Got Response', res.status);
+		return res.json();
+	})
+	.then((body) => {
+		console.log(body);
+	});
+
+// review
+const getResource = async (url) => {
+	const res = await fetch(url);
+	console.log('Got Response', res.status);
+	const body = await res.json();
+	return body;
+};
+
+getResource('https://swapi.co/api/people/1/')
+	.then((body) => {
+		console.log(body);
+	});
