40fe53f1cbbfc94406fa5056e2a96cc3d1344a8f
Tom-Challenger <it.mail.art@gmail.com>
2019-07-16 11:32:54 +0300

    Создаем класс-сервис

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import App from './components/app';
 
-fetch('https://swapi.co/api/people/1/')
-	.then((res) => {
-		console.log('Got Response', res.status);
-		return res.json();
-	})
-	.then((body) => {
-		console.log(body);
-	});
-
-// review
-const getResource = async (url) => {
-	const res = await fetch(url);
-	console.log('Got Response', res.status);
-	const body = await res.json();
-	return body;
-};
-
-getResource('https://swapi.co/api/people/1/')
-	.then((body) => {
-		console.log(body);
-	});
+ReactDOM.render(<App />, document.getElementById('root'));
