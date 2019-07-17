042c43c2cef7953165b1c4ca06e91a43b21aaa44
Tom-Challenger <it.mail.art@gmail.com>
2019-07-17 16:57:35 +0300

    Добавлен обработчик ошибок по fetch

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import SwapiService from '../../services/swapi-service';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 
 import './random-planet.css';
 
@@ -12,7 +13,8 @@ export default class RandomPlanet extends Component {
 
     this.state = {
       planet: {},
-      loading: true
+      loading: true,
+      error: false
     };
 
     this.swapiService = new SwapiService();
@@ -25,21 +27,33 @@ export default class RandomPlanet extends Component {
       loading: false});
   }
 
+  onError = (err) => {
+    this.setState({
+      loading: false,
+      error: true
+    })
+  }
+
   updatePlanet() {
     const id = Math.floor(Math.random()*15)+2;
     this.swapiService
       .getPlanet(id)
-      .then(this.onplanetLoaded);
+      .then(this.onplanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
-    const {planet, loading} = this.state;
+    const {planet, loading, error} = this.state;
 
+    const hasData = !(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator /> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView planet={planet}/> : null;
+    const content = hasData ? <PlanetView planet={planet}/> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
       </div>
